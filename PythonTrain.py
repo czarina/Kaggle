a60b0542db35e70b6d517a40d983d4ae7dd9513e
czarina <zou.christina@gmail.com>
2013-04-21 19:18:42 -0400

    create survival bins:

--- PythonTrain.py
+++ PythonTrain.py
@@ -10,14 +10,12 @@ import numpy as np
 
 #Open up the csv file in to a Python object
 csv_file_object = csv.reader(open('train.csv', 'rU')) 
-header = csv_file_object.next()  #The next() command just skips the 
-                                 #first line which is a header
+header = csv_file_object.next()
+
 data=[]                          #Create a variable called 'data'
 for row in csv_file_object:      #Run through each row in the csv file
     data.append(row)             #adding each row to the data variable
-data = np.array(data) 	         #Then convert from a list to an array
-			         #Be aware that each item is currently
-                                 #a string in this format
+data = np.array(data)            #Then convert from a list to an array
 
 women_only_stats = data[0::,3] == "female" #This finds women  
 
@@ -34,16 +32,47 @@ proportion_men_survived = np.sum(men_onboard) / np.size(men_onboard)
 print 'Proportion of women who survived is %s' % proportion_women_survived
 print 'Proportion of men who survived is %s' % proportion_men_survived
 
+#Create passenger bins on fare (set ceiling to 39), gender, class
+fare_ceiling = 40
+data[data[0::,8].astype(np.float) >= fare_ceiling, 8] = fare_ceiling-1.0
+fare_bracket_size = 10
+number_of_price_brackets = fare_ceiling / fare_bracket_size
+number_of_classes = 3 #There were 1st, 2nd and 3rd classes on board 
+
+# Define the survival table
+survival_table = np.zeros((2, number_of_classes, number_of_price_brackets))
+
+for i in xrange(number_of_classes):       #search through each class
+    for j in xrange(number_of_price_brackets):  #search through each price
+        #find survival data for women in ith class and jth price bracket
+        women_only_stats = data[(data[0::,3] == "female") \
+                       &(data[0::,1].astype(np.float) == i+1) \
+                       &(data[0:,8].astype(np.float) >= j*fare_bracket_size) \
+                       &(data[0:,8].astype(np.float) < (j+1)*fare_bracket_size), 0]                                                                                                                     
+
+        #find survival data for men in ith class and jth price bracket
+        men_only_stats = data[(data[0::,3] != "female")  \
+                       &(data[0::,1].astype(np.float) == i+1) \
+                       &(data[0:,8].astype(np.float) >= j*fare_bracket_size) \
+                       &(data[0:,8].astype(np.float) < (j+1)*fare_bracket_size), 0] 
+
+        survival_table[0,i,j] = np.mean(women_only_stats.astype(np.float)) #Women stats
+        survival_table[1,i,j] = np.mean(men_only_stats.astype(np.float)) #Men stats
+
+survival_table[ survival_table != survival_table ] = 0. #set mean of empty bins to 0
+
+print survival_table
+'''
 test_file_object = csv.reader(open('test.csv', 'rU'))
 header = test_file_object.next()
 
 open_file_object = csv.writer(open("genderbasedmodelpy.csv", "wb"))
 
 for row in test_file_object:
-	if row[2] == 'female':
-		row.insert(0, '1')
-	else:
-		row.insert(0, '0')
-	open_file_object.writerow(row)
-
+    if row[2] == 'female':
+        row.insert(0, '1')
+    else:
+        row.insert(0, '0')
+    open_file_object.writerow(row)
+'''
 
