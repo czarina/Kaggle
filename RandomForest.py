3e7079d6224e4bd9cdc1818b7d361aae76ab2951
czarina <zou.christina@gmail.com>
2013-04-27 22:52:46 -0400

    data filling

--- RandomForest.py
+++ RandomForest.py
@@ -2,7 +2,7 @@
 #scikit-learn package to contruct an ensemble of decision
 #trees with random samplings of training data; predict 
 #survival of Titanic passengers by outputting mode of 
-#classifiers output by the individual trees 
+#classifiers produced by the individual trees 
 
 import csv as csv 
 import numpy as np
@@ -19,19 +19,37 @@ data = np.array(data)
 np.set_printoptions(threshold=np.nan)   
 
 #Prepare data for random trees
-#Convert data into floats when possible; otherwise remove
+#Convert data into floats when possible; remove extraneous rows,
+#and fill in data where possible
 
-#Remove name
-data = [(x[0], x[1], x[3], x[4], x[5], x[6], x[8], x[10]) for x in data]
+#Remove name, cabin, ticket
+data = [(float(x[0]), float(x[1]), x[3], x[4], float(x[5]), float(x[6]), float(x[8]), x[10]) for x in data]
 data = np.array(data)
 
 #Convert gender into 0,1
 data[data[0::,2]=="male", 2] = 0.0
 data[data[0::,2]=="female", 2] = 1.0
 
+#Fill in and reformat Age
+medianList = []
+for x in data[0::,3]:
+	if(x.isdigit()):
+		medianList.append(float(x))
+data[data[0::,3]=='', 3]=float(np.median(medianList))
+data[0::,3]=[float(x) for x in data[0::,3]]
+
 #Convert embarkment to 0, 1, 2
 data[data[0::,7]=="C", 7] = 0.0
 data[data[0::,7]=="Q", 7] = 1.0
 data[data[0::,7]=="S", 7] = 2.0
 
+#Fill in fare
+medianFareList = []
+for x in data[0::,6]:
+	if(x>0):
+		medianFareList.append(float(x))
+data[data[0::,6]=='0.0', 6]=float(np.median(medianFareList))
+
 print data
+
+print np.corrcoef(data[0::,1], data[0::,6])
